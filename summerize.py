6312b47b75a59d4f4bbb689d1639520856192944
Kajaree <kajarees@gmail.com>
2024-11-03 13:15:12 +0100

    updated gpt version and prompts (#2)
    Co-authored-by: dask <kajaree.das@autodesk.com>

--- summerize.py
+++ summerize.py
@@ -1,6 +1,6 @@
 import os
 from GPTAgent import GPTAgent
-from prompts import summary_prompts, review_prompts, category_prompt
+from prompts import summary_prompt, review_prompt, category_prompt
 
 paper_dir = "papers"
 paper_list = os.listdir(paper_dir)
@@ -8,15 +8,11 @@ file_list = [os.path.join(paper_dir, path_to_paper) for path_to_paper in paper_l
 summarize_tool = GPTAgent(file_list)
 author = 'Gnewuch'
 year = '2023'
-prompts = [f'Print the title of the paper which has {author} as its first author and was published in {year}.']
-summarize_tool.askAgent(prompts+summary_prompts)
-
-theme = 'human VS AI research'
-method = 'empirical research'
-review_prompt = [f'Retrieve all relevant articles from the backend database that discuss {theme} and {method}.']
+prompts = f'Print the title of the paper which has {author} as its first author and was published in {year}.'
+summarize_tool.askAgent(prompts + summary_prompt)
 
 user_prompt = input('Please write your query about the papers and our tool will try to answer. Else we will ask for a generic review\n')
 if not user_prompt:
-  user_prompt = ['please summarize empirical research about human VS AI']
-summarize_tool.askAgent(user_prompt + review_prompts)
+  user_prompt = 'Please summarize empirical research about human VS AI'
+summarize_tool.askAgent(user_prompt + review_prompt)
 summarize_tool.askAgent(category_prompt)
